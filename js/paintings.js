76e6b7ed5853e3070c5f349f611a7f2d6ad30a84
phillipcrawford <phillip.crawford3@gmail.com>
2025-05-13 17:01:05 -0600

    change image to cat

--- js/paintings.js
+++ js/paintings.js
@@ -6,7 +6,7 @@ const paintings = [
     dimensions: '24" x 36"',
     year: 2023,
     description: "This piece captures the shifting light of late afternoon on the high ridges of Montana. The artist explores the movement of air and the stillness of rock.",
-    image: "images/painting1.jpg"
+    image: "images/cat1.jpg"
   },
   {
     id: 2,
@@ -15,7 +15,7 @@ const paintings = [
     dimensions: '30" x 40"',
     year: 2022,
     description: "A contemplative view of frozen creeks and quiet woods. The stillness of winter is rendered with subtle color harmonies.",
-    image: "images/painting2.jpg"
+    image: "images/cat2.jpg"
   }
   // Add more paintings as needed
 ];
