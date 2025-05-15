e127c2b1706da79a0176e4de7be0c173a07d5e9e
phillipcrawford <phillip.crawford3@gmail.com>
2025-05-14 23:55:44 -0600

    e

--- js/paintings.js
+++ js/paintings.js
@@ -15,7 +15,7 @@ const paintings = [
     dimensions: '30" x 40"',
     year: 2022,
     description: "A contemplative view of frozen creeks and quiet woods. The stillness of winter is rendered with subtle color harmonies.",
-    image: "imagdes/cat2.jpg"
+    image: "imagees/cat2.jpg"
   }
   // Add more paintings as needed
 ];
