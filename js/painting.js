e11d857cf814d2b7f605ae2b5ae33d900ee9a794
phillipcrawford <phillip.crawford3@gmail.com>
2025-08-23 23:47:24 -0600

    change texts

--- js/painting.js
+++ js/painting.js
@@ -10,7 +10,7 @@ if (painting) {
   document.title = `${painting.title} - Artist Name`;
   document.getElementById("painting-img").src = painting.image;
   document.getElementById("painting-img").alt = painting.title;
-  document.getElementById("painting-title").textContent = painting.title;
+  document.getElementById("paintingd-title").textContent = painting.title;
   document.getElementById("painting-medium").textContent = painting.medium;
   document.getElementById("painting-dimensions").textContent = painting.dimensions;
   document.getElementById("painting-year").textContent = painting.year;
