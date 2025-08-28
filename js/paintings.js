87b6744de875a66e963b741d6debbe10c1e244c3
Phillip Crawford <phillip.crawford3@gmail.com>
2025-08-28 16:03:25 -0600

    fix mixup

--- js/paintings.js
+++ js/paintings.js
@@ -24,7 +24,7 @@ const paintings = [
     dimensions: '30" x 40"',
     year: 2022,
     description: "A coffee table book published in 2019 which has gone through two printings and is now sold out. Bozeman became a subject for me to paint the day a fellow artist spoke about her impending trip to Tuscany to paint its charming towns. I only had to look around on Main Street to find our own very charming town as a subject. I spent five years painting a total of 47 paintings, oil and watercolor, before the text by Bozeman’s outstanding writers followed. Most of the originals have found buyers. There remains about ten that are available.",
-    image: "images/BFTH.jpg"
+    image: "images/hey_day_window_original.jpg"
   },
   {
     id: 4,
@@ -33,7 +33,7 @@ const paintings = [
     dimensions: '30" x 40"',
     year: 2022,
     description: "A coffee table book published in 2019 which has gone through two printings and is now sold out. Bozeman became a subject for me to paint the day a fellow artist spoke about her impending trip to Tuscany to paint its charming towns. I only had to look around on Main Street to find our own very charming town as a subject. I spent five years painting a total of 47 paintings, oil and watercolor, before the text by Bozeman’s outstanding writers followed. Most of the originals have found buyers. There remains about ten that are available.",
-    image: "images/hey_day_window_original.jpg"
+    image: "images/BFTH.jpg"
   },
   {
     id: 5,
