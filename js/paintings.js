7bd62b3b341470b8106210118b38c9e47f3a1f27
phillipcrawford <phillip.crawford3@gmail.com>
2025-08-30 16:18:14 -0600

    resize image

--- js/paintings.js
+++ js/paintings.js
@@ -2,7 +2,7 @@ const paintings = [
   {
     id: 1,
     title: "Yellow Watering Can",
-    medium: "unknown", //Oil on canvas
+    medium: "Oil on canvas"
     dimensions: '40" L x 30" W',
     year: 3000,
     description: "Painting this still-life brought me much pleasure and taught me a few painting insights. The first was the value of beginning with an underpainting, usually an acrylic color, before tackling any details. For this piece I tinted purple with some white and, in this case, the coolness of that color electrified the large central object, a yellow watering can. The yellow began to glow as I painted over the cool purple. For the first time, I had to deal with reflective properties of a metal bowl and with highlights to make the metal metallic. Such joy before my very eyes. Behind the various objects chosen by artist mentor Diana Tremaine, as she staged this particular still-life, she included a patterned woolen blanket as backdrop: its tactile presence in the multi-level arrangement challenged this painter. The Yellow Watering Can is a painting over which a viewer might linger to discover a multitude of objects and the play of yellows, purples and reds.",
@@ -11,7 +11,7 @@ const paintings = [
   {
     id: 2,
     title: "Still Life in Greens",
-    medium: "unknown",  //Oil on canvas
+    medium: "Oil on canvas"
     dimensions: 'unknown', //24" x 36"
     year: 3000,
     description: "While painting in Diana Tremaine’s studio, she suggested I treat a still life I was about to paint to brush strokes that mirrored Cezanne’s. I loved consciously adopting Cezanne’s brush dance on this canvas, his charming ways of accenting color by outlining objects only slightly. Hardly any dead, flat spaces of color. They all shimmer, alive with accented brush marks. His background colors vie for the attention of foreground ones. Color for Cezanne, takes on the importance of form. Intuitively, I too have most often, in early works as well as present ones, sublimated form, and sought to make color dominant.",
@@ -19,17 +19,17 @@ const paintings = [
   },
   {
     id: 3,
-    title: "Bozeman From The Heart",
-    medium: "Oil on linen",
+    title: "Hey Day Window",
+    medium: "Oil on canvas",
     dimensions: '30" x 40"',
     year: 2022,
-    description: "A coffee table book published in 2019 which has gone through two printings and is now sold out. Bozeman became a subject for me to paint the day a fellow artist spoke about her impending trip to Tuscany to paint its charming towns. I only had to look around on Main Street to find our own very charming town as a subject. I spent five years painting a total of 47 paintings, oil and watercolor, before the text by Bozeman’s outstanding writers followed. Most of the originals have found buyers. There remains about ten that are available.",
+    description: "",
     image: "images/hey_day_window_original.jpg"
   },
   {
     id: 4,
     title: "Bozeman From The Heart",
-    medium: "Oil on linen",
+    medium: "Oil on canvas",
     dimensions: '30" x 40"',
     year: 2022,
     description: "A coffee table book published in 2019 which has gone through two printings and is now sold out. Bozeman became a subject for me to paint the day a fellow artist spoke about her impending trip to Tuscany to paint its charming towns. I only had to look around on Main Street to find our own very charming town as a subject. I spent five years painting a total of 47 paintings, oil and watercolor, before the text by Bozeman’s outstanding writers followed. Most of the originals have found buyers. There remains about ten that are available.",
@@ -38,7 +38,7 @@ const paintings = [
   {
     id: 5,
     title: "Dennis",
-    medium: "Oil on linen",
+    medium: "Oil on canvas",
     dimensions: '30" x 40"',
     year: 2022,
     description: "A contemplative view of frozen creeks and quiet woods. The stillness of winter is rendered with subtle color harmonies.",
@@ -47,7 +47,7 @@ const paintings = [
   {
     id: 6,
     title: "Elliot",
-    medium: "Oil on linen",
+    medium: "Oil on canvas",
     dimensions: '30" x 40"',
     year: 2022,
     description: "A contemplative view of frozen creeks and quiet woods. The stillness of winter is rendered with subtle color harmonies.",
@@ -56,7 +56,7 @@ const paintings = [
   {
     id: 7,
     title: "Ferdinand",
-    medium: "Oil on linen",
+    medium: "Oil on canvas",
     dimensions: '30" x 40"',
     year: 2022,
     description: "A contemplative view of frozen creeks and quiet woods. The stillness of winter is rendered with subtle color harmonies.",
